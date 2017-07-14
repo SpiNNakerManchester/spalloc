1bed16f36880421792404a910018cd17394cd9b5
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-07-14 09:08:25 +0100

    Restore build on Python 3.

--- setup.py
+++ setup.py
@@ -29,6 +29,9 @@ setup(
 
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     keywords="spinnaker allocation packing management supercomputer",
 
