49304e320cd6bf25fdbc8d170fcd30a910b77d74
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-07-28 10:25:26 +0100

    all docs tests

--- tests/test_doc_checker.py
+++ tests/test_doc_checker.py
@@ -31,11 +31,6 @@ class TestCfgChecker(unittest.TestCase):
         abs_class_file = os.path.abspath(class_file)
         unittest_dir = os.path.dirname(abs_class_file)
         repo_dir = os.path.dirname(unittest_dir)
-        checker = DocsChecker(
-            #check_init=False,  # 7 errors in 6 files
-            #check_params=False,  # 11 errors in 6 files
-            #check_returns=False,  # 47 errors in 11 files
-            check_properties=False  # 1 errors in 1 files
-            )
+        checker = DocsChecker()
         checker.check_dir(repo_dir)
         checker.check_no_errors()
