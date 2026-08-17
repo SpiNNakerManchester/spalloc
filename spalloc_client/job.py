6067400a2193ce3b0dd4a02b1194bc331d262f56
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-08-17 07:49:52 +0100

    change deprecated imports

--- spalloc_client/job.py
+++ spalloc_client/job.py
@@ -20,9 +20,7 @@ import sys
 import time
 from collections import namedtuple
 from types import TracebackType
-from typing import TypeVar, cast
-
-from typing_extensions import Literal, Self, TypeAlias
+from typing import Literal, Self, TypeAlias, TypeVar, cast
 
 from spinn_utilities.log import FormatAdapter
 from spinn_utilities.typing.json import JsonArray
--- spalloc_client/protocol_client.py
+++ spalloc_client/protocol_client.py
@@ -21,9 +21,7 @@ import socket
 from collections import deque
 from threading import RLock, Thread, current_thread, local
 from types import TracebackType
-from typing import Any, Literal, cast
-
-from typing_extensions import Self
+from typing import Any, Literal, Self, cast
 
 from spinn_utilities.log import FormatAdapter
 from spinn_utilities.typing.json import JsonObject, JsonObjectArray, JsonValue
